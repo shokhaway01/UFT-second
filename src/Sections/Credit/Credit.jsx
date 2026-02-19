781e99060b2320dbe29392beb0e61fa30caddfa0
shokhaway01 <shoxauey@gmail.com>
2026-02-19 23:43:05 +0500

    asdadas

--- src/Sections/Credit/Credit.jsx
+++ src/Sections/Credit/Credit.jsx
@@ -4,7 +4,7 @@ import happyPerson from "../../assets/happyPerson.jpg"
 
 const Credit = () => {
   return (
-    <div className='bg-[#FCD0BA] text-black md:p-0 md:flex-row md:p-15 flex flex-col gap-5 text-3xl  md:p-30 md:text-7xl md:flex justify-center md:justify-around md:items-start md:gap-50'>
+    <div className='bg-[#FCD0BA] text-black md:p-0 md:flex-row md:p-15 flex flex-col gap-5 text-5xl  md:p-30 md:text-7xl md:flex justify-center md:justify-around md:items-start md:gap-50'>
             
             <div className='p-3 text-center md:text-left relative mb-5 mt-10 '>
                 <h1>We believe <br />credit should <br /> be more <br /> fair</h1>
